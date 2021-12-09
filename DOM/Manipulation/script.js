c4a78bc3c4b18a5f033f315c489bf58e10c87461
inwananwar99 <inwananwar9b@gmail.com>
2021-12-09 10:59:31 +0700

    DOM Manipulation Part 1

---
+++ DOM/Manipulation/script.js
@@ -0,0 +1,16 @@
+//Manipulation 1. element.innerHTML
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'Single Coder';
+
+// const section = document.querySelector('section#a');
+// let a = section.getElementsByTagName('a')[0];
+// a.innerHTML = 'Gua hack pake DOM JavaScript wkwk';
+
+//Manipulation 2. element.setAttribute
+// const anchor = document.querySelector('section#a a');
+// anchor.innerHTML='Instagram Inwan Anwar Solihudin';
+// anchor.setAttribute('href','http://instagram.com/inwananwar99');
+
+//Manipulation 3. classList.toggle
+const judul = document.querySelector('.p1');
+judul.classList.toggle('text-center');
