e99436c164195a305eacabe45d08fca5dc8d3557
inwananwar99 <70050023+inwananwar99@users.noreply.github.com>
2022-01-28 08:42:27 +0700

    dom manipulation #1

--- DOM/Manipulation/script.js
+++ DOM/Manipulation/script.js
@@ -6,11 +6,18 @@
 // let a = section.getElementsByTagName('a')[0];
 // a.innerHTML = 'Gua hack pake DOM JavaScript wkwk';
 
+const section1 = document.querySelector('section#b');
+let a = section1.getElementsByTagName('p')[0];
+a.classList.add('judul2');
+a.classList.add('judul3');
+a.classList.add('judul4');
+a.classList.replace('judul2','dom');
+
 //Manipulation 2. element.setAttribute
 // const anchor = document.querySelector('section#a a');
 // anchor.innerHTML='Instagram Inwan Anwar Solihudin';
 // anchor.setAttribute('href','http://instagram.com/inwananwar99');
 
 //Manipulation 3. classList.toggle
-const judul = document.querySelector('.p1');
-judul.classList.toggle('text-center');
+// const judul = document.querySelector('.p1');
+// judul.classList.toggle('text-center');
